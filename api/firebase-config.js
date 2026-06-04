59143f370cb280284e50f64af7511511862a0d79
amcuoficial-lab <amcuoficial@gmail.com>
2026-06-04 16:50:17 -0300

    chore: embed default Firebase config values

--- api/firebase-config.js
+++ api/firebase-config.js
@@ -9,11 +9,11 @@ module.exports = (req, res) => {
     }
 
     res.status(200).json({
-        apiKey: process.env.FIREBASE_API_KEY || "",
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN || "",
-        projectId: process.env.FIREBASE_PROJECT_ID || "",
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "",
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "",
-        appId: process.env.FIREBASE_APP_ID || ""
+        apiKey: process.env.FIREBASE_API_KEY || "AIzaSyBA0MU8Pt0sctRIUeg1uiMdz2Rq0e5aNmU",
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN || "tosco-90f31.firebaseapp.com",
+        projectId: process.env.FIREBASE_PROJECT_ID || "tosco-90f31",
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "tosco-90f31.firebasestorage.app",
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "1079789010898",
+        appId: process.env.FIREBASE_APP_ID || "1:1079789010898:web:46581a797283726d21920b"
     });
 };
